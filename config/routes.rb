0df83c87de40cd2fae6c7857beb0e326a31143dd
jamillessn <jamillerosesison1@gmail.com>
2024-04-06 09:27:42 +0800

    feat: add devise gem, user,stock, and transaction models

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+    
+  namespace :admin do
+    resources :users
+    resources :pending, only: [:index]
+    resources :transactions, only: [:index]
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :stocks 
+  resources :transactions
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
