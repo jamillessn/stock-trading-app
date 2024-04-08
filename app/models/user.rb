477cb4daf0a63517bb25e49212bd38be78677598
jamillessn <jamillerosesison1@gmail.com>
2024-04-08 17:19:28 +0800

    feat: add users controller, transactions and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  default_scope { where(admin:false) }
+       
+  scope :for_approval, -> { where(approved: false) }
+
+  def approve
+    self.approved = true
+    self.approved_at = Time.current
+    save
+  end
+  
 end
