a08b1b3cf077c1efea67d34b8338a184ac1461f1
jamillessn <jamillerosesison1@gmail.com>
2024-04-23 18:16:49 +0800

    refactor: fix route devise_for duplicate

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   root to: 'landing_page#index'
 
-<<<<<<< HEAD
   devise_for :users, controllers: { confirmations: 'users/confirmations' }
-=======
-  devise_for :users
->>>>>>> e0e455d9deb01402d0aec578db5bfa8d2ec7a88c
 
   namespace :admin do
     root to: 'users#index'
@@ -26,10 +22,6 @@ Rails.application.routes.draw do
   end
   resources :transactions
 
-<<<<<<< HEAD
   # Portfolio Route (Place this where you'd like it accessible)
   get '/portfolio', to: 'portfolios#show'
-=======
-
->>>>>>> e0e455d9deb01402d0aec578db5bfa8d2ec7a88c
 end
