7c004ff026411ae05b274ea5362c5a7ad13eb2c0
jamillessn <jamillerosesison1@gmail.com>
2024-04-23 18:27:40 +0800

    refactor: remove duplicate code stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,13 +1,7 @@
 class Stock < ApplicationRecord
-<<<<<<< HEAD
-
   has_many :transactions
   belongs_to :user
 
   validates :shares, numericality: true
   validates :shares, numericality: { greater_than_or_equal_to: 0 }
-=======
-  belongs_to :user
-  validate :shares, comparison: { greater_than_or_equal_to: 0 }
->>>>>>> e0e455d9deb01402d0aec578db5bfa8d2ec7a88c
 end
